b40b55210d143f084a42c8eff96c708e079ebe64
dyy <yuyangd473@gmail.com>
2026-08-03 11:29:51 +0800

    计算器

---
+++ python/calculator.py
@@ -0,0 +1,25 @@
+print("简单计算器")
+
+num1 = float(input("请输入第一个数字: "))
+operator = input("请输入运算符(+ - * /): ")
+num2 = float(input("请输入第二个数字: "))
+
+if operator == "+":
+    result = num1 + num2
+
+elif operator == "-":
+    result = num1 - num2
+
+elif operator == "*":
+    result = num1 * num2
+
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "错误：不能除以0"
+
+else:
+    result = "错误：未知运算符"
+
+print("结果:", result)
